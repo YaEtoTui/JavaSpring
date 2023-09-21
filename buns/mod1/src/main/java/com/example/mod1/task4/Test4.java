ca20e4f422a52a0d42958018a598cba868af8e06
Egor <sazhin2003.egor@gmail.com>
2023-09-21 21:15:17 +0500

    Update Test4.java
    Delete false comment

--- buns/mod1/src/main/java/com/example/mod1/task4/Test4.java
+++ buns/mod1/src/main/java/com/example/mod1/task4/Test4.java
@@ -6,7 +6,7 @@ public class Test4 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
-        Person person = context.getBean("personBean", Person.class); //здесь только возьмутся первые созданные бины в task3
+        Person person = context.getBean("personBean", Person.class);
         person.sayHello();
 
         context.close();
