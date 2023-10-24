2f14b9a29ddcb5ed93a3693112c2f7b8f3af2031
Egor <sazhin2003.egor@gmail.com>
2023-10-24 12:04:02 +0500

    add skillbox -> mod4
    Сразу про страницы, некоторые из них вообще не было заготовлено, то что нашел, использовал. и конечно пролокализировал.

---
+++ skillbox/mod4/mod4/mod4/src/main/java/com/example/MyBookShopApp/controllers/GenresController.java
@@ -0,0 +1,15 @@
+package com.example.MyBookShopApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class GenresController {
+
+    @GetMapping("/genres/index")
+    public String genrePage(Model model){
+        return "genres/index";
+    }
+}
---
+++ skillbox/mod4/mod4/mod4/src/main/java/com/example/MyBookShopApp/controllers/RecentController.java
@@ -0,0 +1,32 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.data.Book;
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Controller
+public class RecentController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public RecentController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks(){
+        return bookService.getBooksData();
+    }
+
+    @GetMapping("/books/recent")
+    public String recentPage(Model model){
+        return "books/recent";
+    }
+}
